8ac197797b4f09ae40e283fbb3b44412093cb2ba
Dom Jina <dominicjina@gmail.com>
2022-09-12 14:08:17 +0100

    Completed Vertex class

--- Vertex.java
+++ Vertex.java
@@ -1,3 +1,16 @@
 public class Vertex {
-    
+    private int incomingDirectedEdges;
+    private int incidentUndirectedEdges;
+    private int outgoingDirectedEdges;
+    private int position;
+
+    Vertex(int position){this.position = position;}
+
+    public int getDegree(){return this.incomingDirectedEdges + this.incidentUndirectedEdges + this.outgoingDirectedEdges;}
+
+    public int getInDegree(){return incomingDirectedEdges;}
+
+    public int getOutDegree(){return outgoingDirectedEdges;}
+
+    public int getPosition(){return this.position;}
 }
